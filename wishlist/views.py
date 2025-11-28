a330df2d1a05b5cddf72bdeffb9d2ee77b58fec0
kakilian <160593671+kakilian@users.noreply.github.com>
2025-11-28 20:08:31 +0100

    Initial commit: setup Django project

---
+++ wishlist/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+def index(request):
+    """Simple index view that renders the project-level `index.html` template."""
+    context = {"title": "Wishlist"}
+    return render(request, "index.html", context)
