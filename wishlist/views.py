c070357d37f101d6fbc60dfc70b1ea9f398fea3b
kakilian <160593671+kakilian@users.noreply.github.com>
2025-11-29 11:16:16 +0100

    feat: base, footer, templates, home-.html

--- wishlist/views.py
+++ wishlist/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def index(request):
     """Simple index view that renders the project-level `index.html` template."""
     context = {"title": "Wishlist"}
-    return render(request, "index.html", context)
+    return render(request, "wishlist/home.html", context)
