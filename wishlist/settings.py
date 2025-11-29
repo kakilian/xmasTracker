dd8bc0691a31f099a7f2bb6879fd15a9b1f42428
Alex <alexander.tastad@hotmail.com>
2025-11-29 21:17:29 +0100

    Add: user registration functionality with form and view

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -133,3 +133,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication settings (Django auth)
+LOGIN_REDIRECT_URL = '/wishlists/'
+LOGOUT_REDIRECT_URL = '/'
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 from .models import Wishlist
 
 
@@ -21,3 +23,15 @@ def wishlist_detail(request, pk):
     """Show a single wishlist with its items."""
     wl = get_object_or_404(Wishlist, pk=pk, user=request.user)
     return render(request, "wishlist/wishlist_detail.html", {"wishlist": wl, "items": wl.items.all()})
+
+
+def register(request):
+    """Simple registration view using Django's UserCreationForm."""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
